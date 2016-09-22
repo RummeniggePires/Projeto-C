3ec67e4b8c97b37184fa5aa1f503439b9a3e2d46
RummeniggePires <rummeniggeps@gmail.com>
2016-09-22 01:04:18 -0300

    Resolucao do problema

--- Exercicio_Avaliativo/Exercicio1/main.cpp
+++ Exercicio_Avaliativo/Exercicio1/main.cpp
@@ -3,10 +3,37 @@
 #include <time.h>
 
 using namespace std;
-int mat[4][4];
-int result[4][4];
 
-void preeche_matriz(){
+class caminho_matriz
+{
+private:
+	int cont, i, j;
+	int mat[4][4];
+	int result[4][4];
+public:
+	caminho_matriz(){
+		cont = 0;
+		i = 0;
+		j = 0;
+	}
+	void zera_result();
+	void preeche_matriz();	
+	void imprime_matriz();
+	void verifica_menor();
+	void imprime_matriz_menor();
+};
+
+void caminho_matriz::zera_result(){
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			result[i][j] = 0;
+		}
+	}
+}
+
+void caminho_matriz::preeche_matriz(){
 	for (int i = 0; i < 4; ++i)
 	{
 		for (int j = 0; j < 4; ++j)
@@ -16,7 +43,7 @@ void preeche_matriz(){
 		}
 	}
 }
-void imprime_matriz(){
+void caminho_matriz::imprime_matriz(){
 	for (int i = 0; i < 4; ++i)
 	{
 	
@@ -33,38 +60,37 @@ void imprime_matriz(){
 
 }
 
-void verifica_menor(){
-	int i =0, j=0, cont=0;
-	while(cont < 6) {
-			    
+void caminho_matriz::verifica_menor(){
 	
+	while( cont < 6 ) {
 
-			if (mat[i+1][j] <= mat[i][j+1])
+		if (mat[i+1][j] <= mat[i][j+1])
+		{
+			result[i+1][j] = mat[i+1][j];
+			if (i < 3)
 			{
-
-				result[i+1][j] = mat[i+1][j];
-				if(j < 3){
-					j++;
-				}else{
-					i++;
-				}
-
+				i++;
 			}else{
-				result[i][j+1] = mat[i][j+1];
-				if(i < 3){
-					i++;
-				}else{
-					j++;
-				}
-			
+				j++;
 			}
-		cont++;
+		
+		}else{
+
+			result[i][j+1] = mat[i][j+1];
+			if (j < 3)
+			{
+				j++;
+			}else{
+				i++;
+			}
 		}
+	
+	cont++;
 	}
+}
 
 
-
-void imprime_matriz_menor(){
+void caminho_matriz::imprime_matriz_menor(){
 	for (int i = 0; i < 4; ++i)
 	{
 	
@@ -82,10 +108,13 @@ int main(int argc, char const *argv[])
 {
 	srand ( (unsigned) time(NULL));
 
-	preeche_matriz();
-	imprime_matriz();
-	verifica_menor();
-	imprime_matriz_menor();
+	caminho_matriz m;
+
+	m.zera_result();
+	m.preeche_matriz();
+	m.imprime_matriz();
+	m.verifica_menor();
+	m.imprime_matriz_menor();
 
 	return 0;
 }
